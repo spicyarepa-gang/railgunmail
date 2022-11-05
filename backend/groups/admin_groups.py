00cd2edff6b16b65927f7e3fd72bcb912e0cbfd2
Ricardo Araujo <58005129+Richardustev@users.noreply.github.com>
2022-11-04 20:22:41 -0400

    04/11/22 - 4

--- backend/groups/admin_groups.py
+++ backend/groups/admin_groups.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from backend.form import AccesoLogin, InsertarContactos
-from backend.models import Admin, Groups
+from backend.models import Admin, Groups, GroupsTypes
 from flask_login import login_required, login_user, logout_user
 from vendors.database import db
 
 groups = Blueprint('groups', __name__)
+
+#GROUPSTYPES VIEW
+from sqlalchemy import desc, asc
+@groups.route('/groupsTypes/view', methods=['GET', 'POST'])
+@login_required
+def view_groups_types():
+    view_groups_types = GroupsTypes.query.order_by(desc('id'))
+    return render_template('groups/view_groups_types.html', data=view_groups_types)
 
 #GROUPS VIEW
 from sqlalchemy import desc, asc
