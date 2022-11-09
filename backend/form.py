867b52fef8fe340be7eb6fe21104e2a4c6173a42
Ricardo Araujo <58005129+Richardustev@users.noreply.github.com>
2022-11-08 20:27:04 -0400

    8/11/22

--- backend/form.py
+++ backend/form.py
@@ -1,22 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, DateField, PasswordField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class AccesoLogin(FlaskForm):
-    user = StringField('Inserta tu usuario', validators=[DataRequired()])
-    passw = PasswordField('Inserta tu contraseña', validators=[DataRequired()])
+    user = StringField('Usuario:', render_kw={"placeholder": "Nombre de usuario"}, validators=[DataRequired()])
+    passw = PasswordField('Contraseña:', render_kw={"placeholder": "Contraseña"}, validators=[DataRequired()])
     entrar = SubmitField('Entrar')
 
 class InsertarContactos(FlaskForm):
-    nombre = StringField('Inserta nombre', validators=[DataRequired()]) 
-    direccion = StringField('Inserta direccion', validators=[DataRequired()])
-    telefono = StringField('Inserta telefono', validators=[DataRequired()])
-    correo = StringField('Inserta correo', validators=[DataRequired()])
-    extension = StringField('Inserta extension', validators=[DataRequired()])
-    cargo = StringField('Inserta cargo', validators=[DataRequired()])
-    departamento = StringField('Inserta departamento', validators=[DataRequired()])
-    submit = SubmitField('Insertar')
+    nombre = StringField('Nombre del contacto:', render_kw={"placeholder": "Nombre del contacto"}, validators=[DataRequired()]) 
+    direccion = TextAreaField('Dirección del contacto:', render_kw={"placeholder": "Dirección del contacto"}, validators=[DataRequired()])
+    telefono = StringField('Teléfono del contacto:', render_kw={"placeholder": "Teléfono del contacto"}, validators=[DataRequired()])
+    correo = StringField('Correo electrónico:', render_kw={"placeholder": "ej: ejemplo@correo.com"}, validators=[DataRequired()])
+    extension = StringField('Extensión del contacto:', render_kw={"placeholder": "Extensión del contacto"}, validators=[DataRequired()])
+    cargo = StringField('Cargo del contacto:', render_kw={"placeholder": "Cargo del contacto"}, validators=[DataRequired()])
+    departamento = StringField('Departamento de procedencia:', render_kw={"placeholder": "Departamento de procedencia"}, validators=[DataRequired()])
+    submit = SubmitField('Añadir contacto')
 
 class InsertarGroupType(FlaskForm):
     nombre = StringField('Insertar el nombre del tipo de grupo', validators=[DataRequired()])
@@ -27,3 +27,8 @@ class InsertarGroup(FlaskForm):
     tipo = SelectField('Tipo de grupo', choices=[], validators=[DataRequired()])
     dependencia = StringField('Dependencia', validators=[DataRequired()])
     submit = SubmitField('Insertar')
+
+class InsertarSubGrupos(FlaskForm):
+    nombre = StringField('Inserta nombre', validators=[DataRequired()]) 
+    submit = SubmitField('Insertar')
+
