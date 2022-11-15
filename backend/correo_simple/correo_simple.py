30cd6567cf5d452f84734daa2981de71cc0ded4a
Ricardo Araujo <58005129+Richardustev@users.noreply.github.com>
2022-11-15 15:17:42 -0400

    base correo simple

--- backend/correo_simple/correo_simple.py
+++ backend/correo_simple/correo_simple.py
@@ -4,5 +4,12 @@ from backend.models import Admin, Groups, GroupsTypes
 from flask_login import login_required, login_user, logout_user
 from vendors.database import db
 
-correo_simple = Blueprint('groupstypes', __name__)
+correo_simple = Blueprint('correo_simple', __name__)
 
+#GROUPS VIEW
+from sqlalchemy import desc, asc
+@correo_simple.route('/correo/groupsview/', methods=['GET', 'POST'])
+@login_required
+def view_correo():
+    view_correo = db.session.query(Groups, GroupsTypes).select_from(Groups).join(GroupsTypes).all()
+    return render_template('correo_simple/correo_simple.html', data=view_correo)
