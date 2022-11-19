c6f983916e3274d9f9b230553e4cc0be57cd3645
Rodrigo <rodrigoqyt@gmail.com>
2022-11-19 07:01:36 -0600

    sesion ilimitada

--- vendors/config.py
+++ vendors/config.py
@@ -1,15 +1,14 @@
 import os 
-#from datetime import timedelta
+from datetime import timedelta
 
 class Config(object):
     SECRET_KEY = os.urandom(16)
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=24)
 
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('CONEXION_DB')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # PERMANENT_SESSION_LIFETIME = timedelta(minutes=3)
-    # SESSION_PERMANENT = True
 
 class ProdConfig(Config):
     DEBUG = False
